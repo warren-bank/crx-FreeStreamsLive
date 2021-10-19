3b84eba5524842c6479b08c3d8b0b28b1f785eee
Warren R Bank <warren.r.bank@gmail.com>
2021-10-18 20:24:57 -0700

    refactor options, add option to redirect WebMonkey to url after Intent

--- webmonkey-userscript/ShowSport.user.js
+++ webmonkey-userscript/ShowSport.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ShowSport.xyz
 // @description  Watch videos in external player.
-// @version      1.0.1
+// @version      1.0.2
 // @match        *://showsport.xyz/*
 // @match        *://*.showsport.xyz/*
 // @match        *://freestreams-live1.com/*
@@ -21,9 +21,14 @@
 // ----------------------------------------------------------------------------- constants
 
 var user_options = {
-  "redirect_to_webcast_reloaded": true,
-  "force_http":                   true,
-  "force_https":                  false
+  "webmonkey": {
+    "post_intent_redirect_to_url":  "about:blank"
+  },
+  "greasemonkey": {
+    "redirect_to_webcast_reloaded": true,
+    "force_http":                   true,
+    "force_https":                  false
+  }
 }
 
 // ----------------------------------------------------------------------------- state
@@ -36,8 +41,8 @@ var state = {
 
 var get_webcast_reloaded_url = function(video_url, vtt_url, referer_url, force_http, force_https) {
   referer_url = referer_url ? referer_url : (referer_url === false) ? false : unsafeWindow.location.href
-  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.force_http
-  force_https = (typeof force_https === 'boolean') ? force_https : user_options.force_https
+  force_http  = (typeof force_http  === 'boolean') ? force_http  : user_options.greasemonkey.force_http
+  force_https = (typeof force_https === 'boolean') ? force_https : user_options.greasemonkey.force_https
 
   var encoded_video_url, encoded_vtt_url, encoded_referer_url, webcast_reloaded_base, webcast_reloaded_url
 
@@ -70,7 +75,7 @@ var redirect_to_url = function(url) {
 
   if (typeof GM_loadUrl === 'function') {
     if (typeof GM_resolveUrl === 'function')
-      url = GM_resolveUrl(url, unsafeWindow.location.href)
+      url = GM_resolveUrl(url, unsafeWindow.location.href) || url
 
     GM_loadUrl(url, 'Referer', unsafeWindow.location.href)
   }
@@ -82,6 +87,19 @@ var redirect_to_url = function(url) {
       unsafeWindow.window.location = url
     }
   }
+}
+
+var process_webmonkey_post_intent_redirect_to_url = function() {
+  var url = null
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'string')
+    url = user_options.webmonkey.post_intent_redirect_to_url
+
+  if (typeof user_options.webmonkey.post_intent_redirect_to_url === 'function')
+    url = user_options.webmonkey.post_intent_redirect_to_url()
+
+  if (typeof url === 'string')
+    redirect_to_url(url)
 }
 
 var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
@@ -108,9 +126,10 @@ var process_video_url = function(video_url, video_type, vtt_url, referer_url) {
     }
 
     GM_startIntent.apply(this, args)
+    process_webmonkey_post_intent_redirect_to_url()
     return true
   }
-  else if (user_options.redirect_to_webcast_reloaded) {
+  else if (user_options.greasemonkey.redirect_to_webcast_reloaded) {
     // running in standard web browser: redirect URL to top-level tool on Webcast Reloaded website
 
     redirect_to_url(get_webcast_reloaded_url(video_url, vtt_url, referer_url))
